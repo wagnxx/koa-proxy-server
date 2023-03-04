60874498936fac5654cc7aa506932e60e6aea1f5
Mr WANG <2362999724@qq.com>
2023-03-04 22:14:23 +0800

    init

---
+++ app.js
@@ -0,0 +1,40 @@
+const Koa = require('koa');
+const path = require('path');
+const app = new Koa();
+const serve = require('koa-static');
+// const serve = require('views-static');
+const proxy = require('koa2-proxy-middleware')
+
+const history = require('connect-history-api-fallback');
+const middleHistory = () => {
+  const middleware = history({
+    index: '/org'
+  });
+  return async (ctx, next)=> {
+    middleware(ctx, null, () => {});
+    await next();
+  };
+};
+
+const options = {
+  targets: {
+    '/api/(.*)': {
+      target: 'http://127.0.0.1:9000',
+      pathRewrite: {
+        '^/api': ''
+      }
+    },
+  }
+}
+
+app.use(middleHistory());
+
+app.use(proxy(options));
+
+// static就是存在前端打包好后的文件夹
+// app.use(serve(path.join(__dirname, '/static'), { extensions: ['html'] }));
+app.use(serve(path.join(__dirname, '/dist'), { extensions: ['html'] }));
+
+app.listen(3000, () => {
+  console.log('server is running, port is 3000');
+}); 
